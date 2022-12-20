d4eeb029ab060023bc54617d974048f16b118fdc
Artem Semirenko <artemiyka95@gmail.com>
2022-12-21 02:23:56 +0300

    add export for inner shadow

--- lib/unlimited_things_flutter.dart
+++ lib/unlimited_things_flutter.dart
@@ -2,6 +2,7 @@ library unlimited_things_dart;
 
 ///beauty folder
 export 'src/flutter/widgets/beauty/divider/pretty_divider.dart';
+export 'src/flutter/widgets/beauty/inner_shadow.dart';
 
 ///layout folder
 export 'src/flutter/widgets/layout/collapsible_builder.dart';
