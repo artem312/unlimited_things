67ca104c5beee1e2348d42bd082a7a3498a4f2ce
Artem Semirenko <artemiyka95@gmail.com>
2022-12-21 02:46:07 +0300

    add json typedefs

--- lib/src/dart/types/function_typedefs.dart
+++ lib/src/dart/types/function_typedefs.dart
@@ -1,18 +1,18 @@
 import 'dart:async';
 
-///callbacks
+//callbacks
 typedef VoidCallback = void Function();
 typedef BoolCallback = bool Function();
 typedef IntCallback = int Function();
 typedef DoubleCallback = double Function();
 
-///async callbacks
+//async callbacks
 typedef AsyncVoidCallback = FutureOr<void> Function();
 typedef AsyncBoolCallback = FutureOr<bool> Function();
 typedef AsyncIntCallback = FutureOr<int> Function();
 typedef AsyncDoubleCallback = FutureOr<double> Function();
 
-///consumers
+//consumers
 typedef VoidConsumer = VoidCallback;
 typedef DoubleConsumer = void Function(double value);
 typedef DoubleConsumer2 = void Function(double value1, double value2);
